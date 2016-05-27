3cb5882b291048763ba8f4a0afd411737b07dc67
Sebastien Delisle <seb0del@gmail.com>
2016-05-27 08:30:45 -0400

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+if [ -z $COMMIT]
+then
+  echo 'You need to specify the $COMMIT environment variable, which is the desired cmangos/mangos-classic commit sha.'
+  exit
+fi
+
 echo "TODO Compiling"
+git clone https://github.com/cmangos/mangos-classic.git /tmp/mangos-classic
+cd /tmp/mangos-classic
+git reset --hard $COMMIT
+
 echo "TODO Deploying"
 
 exec $@
